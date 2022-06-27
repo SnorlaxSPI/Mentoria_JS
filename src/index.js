9004316e90b4c08bb1740b06d520d6a73d0d794c
aflinsjr@icloud.com <aflinsjr@icloud.com>
2022-06-27 11:03:42 -0300

    Criando array e nodemon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-const express = require('express');
+const app = require('express')();
 
-const app = express();
+//const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello, Express with JSON'});
-})
+
+const clients = [
+  { id: 3, nome: 'Alessandro 3', telefone: '14333333333' },
+  { id: 1, nome: 'Alessandro 1', telefone: '14111111111' },
+  { id: 2, nome: 'Alessandro 2', telefone: '14222222222' },
+  { id: 4, nome: 'Alessandro 4', telefone: '14444444444' },
+  
+];
+
+// Request: Tudo que o cliente está mandando vem por essa requisição (ele trás coisas do cliente)
+// Reponse: Tudo que o servidor responde vai por esse objeto, é a resposta do servidor
+
+// Retorno de todos os clientes em uma linha só
+
+app.get('/clients', (request, response) => response.json(clients));
+
+//app.get('/clients', (request, response) => {
+//  response.json(clients);
+//})
+
 
 app.listen(3000);
